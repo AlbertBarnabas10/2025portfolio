cb00e1a2390d0f5a43ba13eaef0f73a83fd5696f
Albert <albertbarnabas10@gmail.com>
2025-03-17 03:12:05 +0700

    home page

--- src/animations/Preloader/Preloader.jsx
+++ src/animations/Preloader/Preloader.jsx
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import './preloader.css';
+import React, { useRef } from "react";
+import "./preloader.css";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 
@@ -8,47 +8,47 @@ gsap.registerPlugin(useGSAP);
 const Preloader = () => {
   const preloaderBgRef = useRef(null);
   const spanRef = useRef(null);
-  
+
   useGSAP(() => {
     const master = gsap.timeline();
-    
+
     gsap.set(spanRef.current, {
-      yPercent: 100, 
+      yPercent: 100,
     });
 
     gsap.set(preloaderBgRef.current, {
       yPercent: 0,
     });
-    
+
     const tl = gsap.timeline({
       defaults: {
-        ease: 'power2.out'
-      }
+        ease: "power2.out",
+      },
     });
 
     tl.to(spanRef.current, {
       yPercent: 0,
-      delay: 0.5
+      delay: 0.5,
     })
-    .to(spanRef.current, {
-      yPercent: -105,
-      delay: 1
-    })
-    .to(preloaderBgRef.current, {
-      yPercent: -100,
-      duration: 1.5,
-      ease: 'power4.out'
-    });
-    
+      .to(spanRef.current, {
+        yPercent: -105,
+        delay: 1,
+      })
+      .to(preloaderBgRef.current, {
+        yPercent: -100,
+        duration: 1.5,
+        ease: "power4.out",
+      });
+
     master.add(tl);
-    
+
     return () => {
       master.kill();
     };
-  }, { scope: "preloader" }); // Optional scope for debugging
+  }); // Optional scope for debugging
 
   return (
-    <div className='preloader'>
+    <div className="preloader">
       <p className="preloader-text custom-font">
         <span ref={spanRef}>Just an ordinary designer.</span>
       </p>

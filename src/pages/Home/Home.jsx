de8645a21a7c97dfb661212266ba3fd6a84b8489
unknown <albert.barnabas@sefasgroup.com>
2025-03-17 08:18:21 +0700

    1703

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Preloader from "../../animations/Preloader/Preloader";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <>
       {/* <Preloader/> */}
+      <Navbar/>
     <div className="home w-full h-screen bg-blue-50">
       <div className="home-content w-full flex items-center h-full">
         <p className="text-5xl">Hello I'm Albert Barnabas.</p>
